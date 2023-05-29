2856697e691bce5444b9c199aef879c866b28998
Neeky <1721900707@qq.com>
2023-05-29 22:19:41 +0800

    ✨feature: 增加快速释放的能力

--- src/sockets/server-socket.py
+++ src/sockets/server-socket.py
@@ -5,6 +5,7 @@ import socket
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind(("127.0.0.1", 9875))
         s.listen(128)
         conn, addr = s.accept()
