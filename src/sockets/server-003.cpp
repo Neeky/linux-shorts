1f9dd433454937199dd497bd93af227bfcbe0c80
Neeky <1721900707@qq.com>
2023-06-15 21:26:50 +0800

    socket

--- src/sockets/server-003.cpp
+++ src/sockets/server-003.cpp
@@ -1,111 +1,103 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <stdio.h>
-#include <netinet/in.h>
-#include <sys/time.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <iostream>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 int main()
 {
+    using namespace std;
     /**
-     * 第一步
+     * 第一步 创建 socket 文件
      */
-    int server_socket, client_socket;
-    socklen_t server_len, client_len;
-    struct sockaddr_in server_address;
-    struct sockaddr_in client_address;
-    int result;
-    fd_set readfds, testfds;
 
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1)
+    {
+        cerr << "create socket fail ." << endl;
+        perror("socket:");
+        return 1;
+    }
+    cout << "server-socket created ." << endl;
+    int reuse = 1;
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, (void *)&reuse, sizeof(reuse));
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEPORT, (void *)&reuse, sizeof(reuse));
     /**
-     * 第二步 创建 socket 文件
+     * 配置 server-socket 的地址信息
      */
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-    /**
-     * 第三步 配置地址信息
-     */
+    sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
     server_address.sin_port = htons(9875);
-    server_len = sizeof(server_address);
-    int reuse = 1;
-    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, (const char *)&reuse, sizeof(int));
-    int bind_res = bind(server_socket, (sockaddr *)&server_address, server_len);
+
+    /**
+     * 第二步执行 bind
+     */
+    int bind_res = bind(server_socket, (sockaddr *)&server_address, sizeof(server_address));
     if (bind_res == -1)
     {
-        printf("bind faild .\n");
+        cerr << "bind socket fail ." << endl;
         perror("bind:");
-        close(server_socket);
-        return 0;
+        return 2;
     }
-    printf("bind .\n");
+    cout << "server-socket bind ." << endl;
 
-    int listen_res = listen(server_socket, 127);
+    /**
+     * 第三步 listen
+     */
+    int listen_res = listen(server_socket, 521);
     if (listen_res == -1)
     {
-        printf("listen fail .\n");
-        perror("listen");
-        close(server_socket);
-        return 0;
+        cerr << "socket listen fail ." << endl;
+        return 3;
     }
-    printf("listen .\n");
+    cout << "server-socket listen ." << endl;
 
-    FD_ZERO(&readfds);
-    FD_SET(server_socket, &readfds);
-
+    sockaddr_in client_address;
+    socklen_t addrlen = sizeof(client_address);
     while (true)
     {
-        char ch;
-        int fd;
-        int nread;
-
-        testfds = readfds;
-        printf("server waiting \n");
-
-        result = select(FD_SETSIZE, &testfds, (fd_set *)NULL, (fd_set *)NULL, (struct timeval *)NULL);
-
-        if (result < 1)
+        int client_socket = accept(server_socket, (sockaddr *)&client_address, &addrlen);
+        if (client_socket == -1)
         {
-            perror("server");
-            exit(1);
+            cerr << "sock accept fail ." << endl;
+            close(client_socket);
+            return 4;
         }
-
-        // 因为最多就只能是有 FD_SETSIZE 个
-        for (fd = 0; fd < FD_SETSIZE; fd++)
+        cout << "accept new cleint fd: %d " << client_socket << endl;
+        /**
+         * 先读一个字符，然后再写一个新字符回去
+         */
+        char buff[1024] = {0};
+        int recv_len = recv(client_socket, buff, 1024, 0);
+        if (recv_len == -1)
         {
-            if (FD_ISSET(fd, &testfds))
-            {
-                if (fd == server_socket)
-                {
-                    client_len = sizeof(client_address);
-                    client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_len);
-                    FD_SET(client_socket, &readfds);
-                    printf("adding client fd %d \n", client_socket);
-                }
-                else
-                {
-                    ioctl(fd, FIONREAD, &nread);
-                    if (nread == 0)
-                    {
-                        close(fd);
-                        FD_CLR(fd, &readfds);
-                        printf("removing client on fd %d \n", fd);
-                    }
-                    else
-                    {
-                        read(fd, &ch, 1);
-                        printf("server client on fd %d \n", fd);
-                        ch++;
-                        write(fd, &ch, 1);
-                    }
-                }
-            }
+            cerr << "recv-len: " << recv_len << " fail ." << endl;
+            close(client_socket);
+            close(server_socket);
+            return 2;
         }
+        cout << "revc from client : " << buff << endl;
+        /**
+         * 写一个字符回 client
+         */
+        buff[0]++;
+        int send_len = send(client_socket, buff, 1, 0);
+        if (send_len == -1)
+        {
+            cerr << "send fail ." << endl;
+            close(client_socket);
+            close(server_socket);
+            return 2;
+        }
+        cout << "send " << buff << " to client ." << endl;
+
+        close(client_socket);
+        cout << "client socket close ." << endl;
+        /** end while */
     }
+
+    close(server_socket);
+    cout << "server-socket close ." << endl;
 
     return 0;
 }
